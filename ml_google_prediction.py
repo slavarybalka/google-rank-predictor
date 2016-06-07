d8d0cfad684fc470449b16da16ed3d64b8014de4
slavarybalka <slavarybalko@gmail.com>
2016-06-07 16:45:13 -0500

    initial commit

---
+++ ml_google_prediction.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# python 2.7
+# using supervised learning to predict the position
+# of the page in Google for a given keyword
+
+
+'''
+1. # of external backlinks
+2. # of social signals
+3. # page size
+4. # page loading speed
+5. # outbound backlinks
+'''
+
+
+def get_external_backlinks:
+end
+
+def get_social_signals:
+end
+
+def get_page_size:
+end
+
+def get_page_loading_speed:
+end
+
+def get_outbound_backlinks:
+end
