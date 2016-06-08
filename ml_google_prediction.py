340c3bc5d51366e10c705b1471185b84afd061b3
slavarybalka <slavarybalko@gmail.com>
2016-06-08 16:46:07 -0500

    added heatmap from seaborn

--- ml_google_prediction.py
+++ ml_google_prediction.py
@@ -13,12 +13,68 @@
 5. # outbound backlinks
 '''
 import numpy as np
-import matplotlib.pyplot as pl
-from sklearn import datasets
+import pandas as pd
+import matplotlib as plt
+
+import seaborn as sns
+
+from sklearn.datasets import load_boston
+from sklearn.cross_validation import cross_val_score
 from sklearn.tree import DecisionTreeRegressor
 
+df = pd.read_csv("C:\Data Science\pw_ml_dataset.csv")
+cols = ['Rank',
+        'Exact match in Title',
+        'Exact Match Meta Desc',
+        'Word Count',
+        'Inlinks',
+        'Outlinks',
+        'External Outlinks',
+        'Social Signals',
+        'Response Time',
+        'URL Length']
 
-TEST_FEATURES = [[1, 18, 36, 23, 2]] #sample metrics of the page we are testing
+#cols = ['rank', 'test']
+def compare_features(cols):
+    sns.set(style='whitegrid', context='notebook')
+    sns.pairplot(df[cols], size=2)
+
+def show_heatmap(cols):
+    cm = np.corrcoef(df[cols].values.T)
+    sns.set(font_scale=1.5)
+    hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols, xticklabels=cols)
+    
+
+#print df.head()
+show_heatmap(cols)
+compare_features(cols)
+
+#plt.show()
+'''
+def lin_regplot(X, y, model):
+    plt.scatter(X, y, c='blue')
+    plt.plot(X, model.predict(X), color='red')
+    return None
+
+#TEST_FEATURES_TEMPLATE = [[external_backlinks, social_signals, page_size, page_loading_time, outbound_backlinks]]
+#TEST_FEATURES = [[1, 18, 36, 23, 2]] #sample metrics of the page we are testing
+
+
+
+X = df[['Size']].values
+y = df[['Word Count']].values
+
+tree = DecisionTreeRegressor(max_depth = 3)
+tree.fit(X, y)
+sort_idx = X.flatten().argsort()
+lin_regplot(X[sort_idx], y[sort_idx], tree)
+plt.xlabel('Page Size')
+plt.ylabel('Word Count')
+plt.show()
+
+print X, Y
+#print df.head()
+
 
 def get_external_backlinks():
     end
@@ -30,8 +86,9 @@ def get_page_size():
     end
 
 
-def get_page_loading_speed():
+def get_page_loading_time():
     end
 
 def get_outbound_backlinks():
     end
+'''
